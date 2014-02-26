f4259087154caa81efcffb6def84a39823d48076
Anthony <antman164@msn.com>
2014-02-26 21:54:55 +0000

    Update world function mostly completed

--- engine.py
+++ engine.py
@@ -17,20 +17,23 @@ class Engine(Thread):
 				message = self._messages_from_gui.pop(0)
 				print message[0]
 
-				if (message[0]) == "load_brain":
+				if (message[0]) == "load brain":
 					ant_brain = brain.Brain.parse_brain(message[1])
 					if (message[2]) == "red":
 						self._red_brain = ant_brain
 					elif  (message[2]) == "black":
 						self._black_brain = ant_brain
 
-				if (message[0]) == "load_world":
+				if (message[0]) == "load world":
 					self._world = self._parse_world(message[1])
 
-				if (message[0]) == "generate_world":
+				if (message[0]) == "generate world":
 					pass #TODO
-				if (message[0]) == "step_world":
-					pass
+				if (message[0]) == "step world":
+					for ant in self._red_ants:
+						self._update_ant(ant)
+					for ant in self._black_ants:
+						self._update_ant(ant)
 			time.sleep(.001)
 
 	BLACK = "-"
@@ -38,7 +41,7 @@ class Engine(Thread):
 	FOOD = (1, 9)
 	NONE = "."
 	RED = "+"
-	
+
 
 	def make_cell(ant=None, rock=False, markers=[], foods=0):
 		return {"ant":ant, "rock":rock, "markers":markers, "foods":foods}
@@ -99,6 +102,114 @@ class Engine(Thread):
 			self._gui.change_world_details("Expected perimeter at end.")
 
 		return parsed
+
+
+	def _update_ant(self, ant):
+		"""Returns updated world after each intruction"""
+
+		response[0] = ant.update_brain(self._world)
+		x, y = ant._position
+
+		if response[0] == "Mark":
+			grid[x][y].markers.append(response[1])
+
+		elif response[0] == "Unmark":
+			if response[1] in grid[x][y].markers:
+				grid[x][y].markers.remove(response[1])
+
+		elif response[0] == "Pickup":
+			if grid[x][y].foods > 0:
+				ant._has_food = True
+				grid[x][y].foods -= 1
+
+		elif response[0] == "Drop":
+			if ant._has_food == True:
+				ant._has_food = False
+				grid[x][y].foods += 1
+
+		elif response[0] == "Move":
+			new_x, new_y = self._apply_move(ant, ant._direction, x, y)
+
+			if grid[new_x][new_y].rock == False && grid[new_x][new_y].ant == None:
+				grid[x][y].ant = None
+				grid[new_x][new_y].ant = ant
+				ant._position = new_x, new_y
+				if self._is_alive(ant, new_x, new_y) == False:
+					grid[new_x][new_y].ant = None
+					# remove ant
+	
+		elif response[0] == "Turn-Left":
+			if ant._direction < 5:
+				ant._direction += 1
+			else:
+				ant._direction = 0
+
+		elif response[0] == "Turn-Right":
+			if ant._direction > 0:
+				ant._direction -= 1
+			else:
+				ant._direction = 5
+
+
+	def _apply_move(self, d, x, y):
+		"""Returns the correct move"""
+		
+		if y % 2 == 0:
+			if d == 0:
+				x += 1
+
+			elif d == 1:
+				y += 1
+
+			elif d == 2:
+				x -= 1
+				y += 1
+
+			elif d == 3:
+				x -= 1
+
+			elif d == 4:
+				x -= 1
+				y -= 1
+
+			elif d == 5:
+				y -= 1
+
+		else:
+			if d == 0:
+				x += 1
+
+			elif d == 1:
+				x += 1
+				y += 1
+
+			elif d == 2:
+				y += 1
+
+			elif d == 3:
+				x -= 1
+
+			elif d == 4:
+				y -= 1
+
+			elif d == 5:
+				x += 1
+				y -= 1
+
+		return x, y
+
+
+	def _is_alive(self, ant, x, y):
+		"""Checks if ant is still alive after move"""
+
+		if y % 2 == 0:
+			if sum([0 if grid[xx][yy].ant == None else 1 for xx, yy in ((x+1,y), (x-1,y), (x-1,y+1),(x,y+1),(x-1,y-1),(x,y-1))]) >= 5:
+				return False
+		else:
+			if sum([0 if grid[xx][yy].ant == None else 1 for xx, yy in ((x+1,y), (x-1,y), (x+1,y+1),(x,y+1),(x+1,y-1),(x,y-1))]) >= 5:
+				return False
+
+		return True
 
 
 	def parse_grid(self, path):
