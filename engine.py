37420205783785f31f5454883c326f4677eee546
Anthony <antman164@msn.com>
2014-02-26 19:47:39 +0000

    started angin.py

--- engine.py
+++ engine.py
@@ -16,4 +16,19 @@ class Engine(Thread):
 			if len(self._messages_from_gui) > 0:
 				message = self._messages_from_gui.pop(0)
 				print message[0]
+
+				if (message[0]) == "load_brain";
+					ant_brain = brain.Brain.parse_brain(message[1])
+					if (message[2]) == "red";
+						self._red_brain = ant_brain
+					elif  (message[2]) == "black";
+						self._black_brain = ant_brain
+
+				if (message[0]) == "load_world";
+					self._world = self._parse_world(message[1])
+
+				if (message[0]) == "generate_world";
+					pass #TODO
+				if (message[0]) == "step_world";
+
 			time.sleep(.001)
